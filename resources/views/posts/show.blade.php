d51368a3072a4c557d045f57cb57dcbc9ce9cc8a
Salaheddine Hadraoui <hadraouisalah8@gmail.com>
2025-03-20 16:52:27 +0000

    rename the home blade template to index

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,7 @@
         <h1 class="font-2xl">Show Post : {{ $ShowPost['title'] }}</h1>
         <p>{{ $ShowPost['content'] }}</p>
         <p>{{ $ShowPost['slug'] }}</p>
-        @if ($ShowPost['active'] === 0)
+        @if ( $ShowPost['active'] === 0 )
             <p class="text-green-500">
                 <span cl ass="w-10 h-10 rounded-full bg-green-500"></span>
                 True
